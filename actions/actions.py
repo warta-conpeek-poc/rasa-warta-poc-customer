181db3898e925099b4cb078bdbc418d31125d067
Adam Kuśmirek <adam.kusmirek@conpeek.com>
2022-02-04 14:14:18 +0100

    progress

--- actions/actions.py
+++ actions/actions.py
@@ -3,7 +3,10 @@ import random
 from typing import Any, Text, Dict, List
 #
 from rasa_sdk import Action, Tracker
-from rasa_sdk.events import AllSlotsReset, SlotSet
+from rasa_sdk.events import AllSlotsReset
+from rasa_sdk.events import SlotSet
+from rasa_sdk.events import SessionStarted
+from rasa_sdk.events import ActionExecuted
 from rasa_sdk.events import Restarted
 from rasa_sdk.events import EventType
 from rasa_sdk.events import FollowupAction
@@ -20,6 +23,50 @@ import pytz
 
 logger = logging.getLogger(__name__)
 
+class ActionSessionStart(Action):
+    def name(self) -> Text:
+        return "action_session_start"
+
+    @staticmethod
+    def fetch_slots(tracker: Tracker, ) -> List[EventType]:
+        """Fetch SlotSet events from tracker and carry over key, value and metadata."""
+        return [
+            SlotSet(key=event.key, value=event.value, metadata=event.metadata)
+            for event in tracker.applied_events()
+            if isinstance(event, SlotSet)
+        ]
+
+
+    async def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[EventType]:
+
+        events = [SessionStarted()]
+        metadata = tracker.latest_message["metadata"]
+        logging.critical(metadata)
+
+        # # any slots that should be carried over should come after the
+        # # `session_started` event`
+        # events.extend(self._slot_set_events_from_tracker(tracker))
+        #
+        # # Grab slots from metadata
+        # message_metadata = []
+        # for e in tracker.events[::-1]:
+        #   # Does this tracker event have metadata?
+        #     if "metadata" in e and e["metadata"] != None:
+        #         message_metadata = e["metadata"]
+        #         # Does this metadata have slots?
+        #         if message_metadata and "slots" in message_metadata:
+        #             for key, value in message_metadata["slots"].items():
+        #                 logger.info(f"{key} | {value}")
+        #                 if value is not None:
+        #                     events.append(SlotSet(key=key, value=value))
+        #             break
+        # if len(message_metadata) == 0:
+        #     logger.warn(f"session_start but no metadata, tracker.events: {tracker.events}")
+        #
+        # # an `action_listen` should be added at the end as a user message follows
+        # events.append(ActionExecuted("action_listen"))
+
+        return events
 
 class ActionAllSlotsReset(Action):
 
