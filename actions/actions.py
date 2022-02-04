8885f5b95b312843663895468305ec6153e68d64
Adam Kuśmirek <adam.kusmirek@conpeek.com>
2022-02-04 12:43:18 +0100

    progress

---
+++ actions/actions.py
@@ -0,0 +1,67 @@
+import json
+import random
+from typing import Any, Text, Dict, List
+#
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import AllSlotsReset, SlotSet
+from rasa_sdk.events import Restarted
+from rasa_sdk.events import EventType
+from rasa_sdk.events import FollowupAction
+from rasa_sdk.executor import CollectingDispatcher
+#
+#
+from rasa_sdk.types import DomainDict
+import re
+import logging
+import datetime
+import pytz
+
+
+
+logger = logging.getLogger(__name__)
+
+
+class ActionAllSlotsReset(Action):
+
+    def name(self) -> Text:
+        return "action_all_slots_reset"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text="Wszystkie dane zostały zresetowane.")
+        return [AllSlotsReset()]
+
+class ActionRestarted(Action):
+
+    def name(self) -> Text:
+        return "action_restarted"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text="Konwersacja została zrestartowana.")
+        return [Restarted()]
+
+class ActionLowConfidence(Action):
+
+    def name(self) -> Text:
+        return "action_low_confidence"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        utter_list = []
+        metadata = tracker.latest_message["metadata"]
+        logging.info(metadata)
+        if "stop_playback_date" in metadata and not metadata["stop_playback_date"]:
+            utter_list.append("")
+        else:
+            utter_list.append("Czy możesz powtórzyć?")
+        text_message = random.choice(utter_list)
+        custom_message = {
+            "blocks": [
+                {
+                    "text": text_message,
+                }
+            ]
+        }
+        if tracker.get_latest_input_channel() == "conpeek-voice":
+            dispatcher.utter_message(json_message=custom_message)
+        else:
+            dispatcher.utter_message(text=text_message)
+
